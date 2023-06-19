fbb9db668850dcf63fc47920fbd025cb9621d42f
leonardnarvaez <79395144+leonardnarvaez@users.noreply.github.com>
2023-06-19 11:02:27 +0800

    Initial Commit (NodeJS Starter Project)

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log('Hello NodeJS!');
+var nameArr = ["Leonard", "Narvaez"];
+for (var i = 0; i <= nameArr.length - 1; i++) {
+    console.log(nameArr[i]);
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+console.log('Hello NodeJS!')
+const nameArr: string[] = ['Leonard', 'Narvaez']
+
+for (let i = 0; i <= nameArr.length - 1; i++) {
+    console.log(nameArr[i])
+}
